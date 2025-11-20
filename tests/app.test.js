9fdcf60f3aa6e28e2a27669efcf3cc90dc4bbdf2
sannipoykio <ag2465@student.jamk.fi>
2025-11-20 14:05:10 +0200

    test environment build

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+const Laskin = require('../public/app.js');
+
+describe('taskin luonnin testaus', 
+
+  it('should do stuff', function () {
+
+  it('should do more stuff', 
+  });
+});
