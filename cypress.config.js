ed2b68979e6a77c0518b16a8eeaeea9d02e7da78
sannipoykio <ag2465@student.jamk.fi>
2025-12-11 13:51:15 +0200

    cypressin asennus

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
+    // Cypress-konfiguraatiot
+    baseUrl: 'http://localhost:5173', // sovelluksen sijaintiurl kehitykympäristössä
+    specPattern: 'testaus-todo/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}', // polku ja tiedostonimimuoto, jolla Cypress etsii testitiedostot
+    supportFile: 'testaus-todo/cypress/support/e2e.js', // polku ja tiedostonimimuoto, jolla Cypress etsii supportFilen
+    fixturesFolder: 'testaus-todo/cypress/fixtures', // polku ja tiedostonimimuoto, jolla Cypress etsii fixtures-kansion
+  },
+});
