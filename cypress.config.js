027500c8e0e35fc7cfe87e3015b28c449ae364cc
sannipoykio <ag2465@student.jamk.fi>
2025-12-17 13:55:16 +0200

    muutos cypress configiin

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Cypress-konfiguraatiot
     baseUrl: 'http://localhost:5173', // sovelluksen sijaintiurl kehitykympäristössä
     specPattern: 'testaus-todo/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}', // polku ja tiedostonimimuoto, jolla Cypress etsii testitiedostot
-    supportFile: 'testaus-todo/cypress/support/e2e.js', // polku ja tiedostonimimuoto, jolla Cypress etsii supportFilen
-    fixturesFolder: 'testaus-todo/cypress/fixtures', // polku ja tiedostonimimuoto, jolla Cypress etsii fixtures-kansion
+    // supportFile: 'testaus-todo/cypress/support/e2e.js', // polku ja tiedostonimimuoto, jolla Cypress etsii supportFilen
+    // fixturesFolder: 'testaus-todo/cypress/fixtures', // polku ja tiedostonimimuoto, jolla Cypress etsii fixtures-kansion
   },
 });
