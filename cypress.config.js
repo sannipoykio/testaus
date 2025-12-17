98352362d5f15d047e465f9926f44493c38cc416
sannipoykio <ag2465@student.jamk.fi>
2025-12-17 14:02:15 +0200

    kokeilua

--- cypress.config.js
+++ cypress.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
 
     // Cypress-konfiguraatiot
     baseUrl: 'http://localhost:5173', // sovelluksen sijaintiurl kehitykympäristössä
-    specPattern: 'testaus-todo/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}', // polku ja tiedostonimimuoto, jolla Cypress etsii testitiedostot
-    // supportFile: 'testaus-todo/cypress/support/e2e.js', // polku ja tiedostonimimuoto, jolla Cypress etsii supportFilen
-    // fixturesFolder: 'testaus-todo/cypress/fixtures', // polku ja tiedostonimimuoto, jolla Cypress etsii fixtures-kansion
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}', // polku ja tiedostonimimuoto, jolla Cypress etsii testitiedostot
+    supportFile: 'cypress/support/e2e.js', // polku ja tiedostonimimuoto, jolla Cypress etsii supportFilen
+    fixturesFolder: 'cypress/fixtures', // polku ja tiedostonimimuoto, jolla Cypress etsii fixtures-kansion
   },
 });
